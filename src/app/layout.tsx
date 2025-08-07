ffb4068c86e83fe3f525979ed8c02c78f5a11194
Clarence Ho <ho.clarence@gmail.com>
2025-08-07 11:56:43 +0800

    Added Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { GoogleAnalytics } from '@next/third-parties/google';
 import { Source_Sans_3, Manrope } from "next/font/google";
+import { Analytics } from '@vercel/analytics/next';
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -50,6 +51,7 @@ export default function RootLayout({
         <Header />
         <main>
           {children}
+          <Analytics />
         </main>
         <Footer />
       </body>
